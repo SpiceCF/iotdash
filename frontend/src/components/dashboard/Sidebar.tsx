af8cbf8b19dbc9bd7006c50c83b8581b5529ce68
SpiceCF <chanachai.s@codefin.io>
2024-10-27 16:16:32 +0700

    fix iconify deps for frontend

--- frontend/src/components/dashboard/Sidebar.tsx
+++ frontend/src/components/dashboard/Sidebar.tsx
@@ -1,15 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import {
-  Bell,
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  ShoppingCart,
-  Users,
-} from 'lucide-react';
+import { Bell, ChartLine, Package2, SatelliteDish } from 'lucide-react';
 
 import { Button } from '../ui/button';
 import { NavLink } from './NavLink';
@@ -23,18 +15,19 @@ export function Sidebar() {
             <Package2 className="h-6 w-6" />
             <span className="">Acme Inc</span>
           </Link>
-          <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+          <Button
+            variant="outline"
+            size="icon"
+            className="relative ml-auto h-8 w-8"
+          >
             <Bell className="h-4 w-4" />
             <span className="sr-only">Toggle notifications</span>
           </Button>
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <NavLink href="#" icon={Home} label="Dashboard" />
-            <NavLink href="#" icon={ShoppingCart} label="Orders" badge="6" />
-            <NavLink href="#" icon={Package} label="Products" active />
-            <NavLink href="#" icon={Users} label="Customers" />
-            <NavLink href="#" icon={LineChart} label="Analytics" />
+            <NavLink href="#" icon={ChartLine} label="Dashboard" active />
+            <NavLink href="#" icon={SatelliteDish} label="Sensor" badge="6" />
           </nav>
         </div>
         <div className="mt-auto p-4"></div>
